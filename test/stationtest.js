6fc9351ca5d6b4066c073a7745971f8c3bf1d3c0
unknown <atev0001@FIHEL-3WTMP12.synchronoss.net>
2016-02-26 17:03:21 +0200

    previosly omitted cleanup

--- test/stationtest.js
+++ test/stationtest.js
@@ -3,11 +3,6 @@ var stationA = {x:0, y:0, reach:2};
 var stationB = {x:0, y:0, reach:1};
 var zeroZero = {x:0, y:0};
 
-var referenceStationList = {
-	one: {x:0, y:0, reach:2},
-	two: {x:0, y:0, reach:2},
-	three: {x:0, y:0, reach:2}
-}
 
 var sortingTestTemplate = function(test, list, deviceLocation, expected){
 	test.expect(1);
@@ -19,11 +14,11 @@ var sortingTestTemplate = function(test, list, deviceLocation, expected){
 }
 
 exports.findNothingInReach = function (test) {
-	sortingTestTemplate(test, [stationA], {"x":3, "y": 3}, undefined);
+	sortingTestTemplate(test, [stationA], {x:3, y: 3}, undefined);
 }
 
 exports.findOneStationInReach = function (test) {
-	sortingTestTemplate(test, [stationA], {"x":0, "y": 0}, stationA);
+	sortingTestTemplate(test, [stationA], {x:0, y: 0}, stationA);
 }
 
 exports.findFirstStationInReach = function (test) {
