7914c8297b9fc4ee9d856de3abb0601b5f14d49c
unknown <atev0001@FIHEL-3WTMP12.synchronoss.net>
2016-02-26 14:00:44 +0200

    basic geometry calculations

--- geometry.js
+++ geometry.js
@@ -1,3 +1,15 @@
 module.exports.distance = function(pointA, pointB, callback){
-	callback(null, 1);
+
+	var sumOfSquares = Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2);
+	var distance = Math.sqrt(sumOfSquares);
+
+	callback(null, distance);
+}
+
+module.exports.radiusToDistance = function(reach, distance, callback){
+	if( reach>distance){
+		callback(null, Math.pow(reach-distance,2));
+	}else{
+		callback(null, 0);
+	}
 }
--- test/geometrytest.js
+++ test/geometrytest.js
@@ -1,10 +1,49 @@
-exports.distance = function (test) {
-
-	var geometry = require('../geometry');
-
+var geometry = require('../geometry');
+var distanceTestTemplate = function(test, pointA, pointB, expected){
 	test.expect(1);
-	geometry.distance({},{}, function(error, distance){
-		test.equals(1, distance, "just pass");
+	geometry.distance(pointA, pointB, function(error, distance){
+		test.equals(expected, distance, "got "+distance);
 		test.done();
 	});
+};
+
+var powerTestTemplate = function(test, reach, distance, expected){
+	test.expect(1);
+	geometry.radiusToDistance(reach,distance, function(error, power){
+		test.equals(expected,power, "got "+power);
+		test.done();
+	});
+
+}
+
+exports.distanceOneSquareRoot = function (test) {
+	distanceTestTemplate(test, {"x":0, "y":0},{"x":1, "y":1}, Math.sqrt(2));
+}
+
+exports.distanceOneSquareRootInverse = function (test) {
+	distanceTestTemplate(test, {"x":1, "y":1}, {"x":0, "y":0}, Math.sqrt(2));
+}
+
+exports.distanceFiveSquareRoot = function (test) {
+	// just to see that the math scales upwards from one
+	distanceTestTemplate(test, {"x":1, "y":1}, {"x":2, "y":3}, Math.sqrt(5));
+}
+
+exports.distanceZeroSquareRoot = function (test) {
+	distanceTestTemplate(test, {"x":0, "y":0},{"x":0, "y":0}, 0);
+}
+
+exports.expectingOnePower = function (test){
+	powerTestTemplate(test, 2, 1, 1); 
+}
+
+exports.expectingFourPower = function (test){
+	powerTestTemplate(test, 3, 1, 4); 
+}
+
+exports.reachSameAsThanDistance = function (test){
+	powerTestTemplate(test, 3, 3, 0); 
+}
+exports.reachSmallerThanDistance = function (test){
+	powerTestTemplate(test, 3, 4, 0); 
 }
